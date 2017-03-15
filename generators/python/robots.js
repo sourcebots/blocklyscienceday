de61564ec6ad043f430ed07ced03b63b80752eb6
Anton Nikitin <antonnikitin@live.co.uk>
2017-03-15 23:12:02 +0000

    modified python code generators

--- generators/python/robots.js
+++ generators/python/robots.js
@@ -1,22 +1,26 @@
 var move_forward_power = 120;
+var sleep_time = 1;
 
 Blockly.Python['turn_left'] = function(block) {
   // TODO: Assemble Python into code variable.
-  var code = 'setMotorPower(value)'.replace("value", move_forward_power);
+  var code = 'setMotorPower(m[0] ,value)\nsleep(sleep_time)'.replace("value", move_forward_power).
+                replace("sleep_time", sleep_time);
   alert(code);
   return code;
 };
 
 Blockly.Python['turn_right'] = function(block) {
   // TODO: Assemble Python into code variable.
-  var code = 'print("Hello World")\n';
+  var code = 'setMotorPower(m[1] ,value)\nsleep(sleep_time)'.replace("value", move_forward_power).
+                replace("sleep_time", sleep_time);
   return code;
 };
 
 Blockly.Python['go_forward'] = function(block) {
   var value_forward_seconds = Blockly.Python.valueToCode(block, 'forward_seconds', Blockly.Python.ORDER_ATOMIC);
   // TODO: Assemble Python into code variable.
-  var code = 'print("Hello World")\n';
+  var code = 'setMotorPower(value)\nsleep(sleep_time)'.replace("value", move_forward_power).
+                replace("sleep_time", sleep_time);
   return code;
 };
 
@@ -24,6 +28,7 @@ Blockly.Python['go_backward'] = function(block) {
   var number_go_backward = block.getFieldValue('Go Backward');
   var value_backward_seconds = Blockly.Python.valueToCode(block, 'backward_seconds', Blockly.Python.ORDER_ATOMIC);
   // TODO: Assemble Python into code variable.
-  var code = 'print("Hello World")\n';
+ var code = 'setMotorPower(value)\nsleep(sleep_time)'.replace("value", move_forward_power).
+                replace("sleep_time", sleep_time);
   return code;
 };
