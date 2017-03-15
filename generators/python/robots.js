aaccb055699755c8e3782063d445937ac7426517
Anton Nikitin <antonnikitin@live.co.uk>
2017-03-15 22:45:19 +0000

    code saving

--- generators/python/robots.js
+++ generators/python/robots.js
@@ -1,19 +1,20 @@
 Blockly.Python['turn_left'] = function(block) {
   // TODO: Assemble Python into code variable.
-  var code = 'print("Hello World")';
+  var code = 'setMotorPower(value)'.replace("value", 200);
+  alert(code);
   return code;
 };
 
 Blockly.Python['turn_right'] = function(block) {
   // TODO: Assemble Python into code variable.
-  var code = 'print("Hello World")';
+  var code = 'print("Hello World")\n';
   return code;
 };
 
 Blockly.Python['go_forward'] = function(block) {
   var value_forward_seconds = Blockly.Python.valueToCode(block, 'forward_seconds', Blockly.Python.ORDER_ATOMIC);
   // TODO: Assemble Python into code variable.
-  var code = 'print("Hello World")';
+  var code = 'print("Hello World")\n';
   return code;
 };
 
@@ -21,6 +22,6 @@ Blockly.Python['go_backward'] = function(block) {
   var number_go_backward = block.getFieldValue('Go Backward');
   var value_backward_seconds = Blockly.Python.valueToCode(block, 'backward_seconds', Blockly.Python.ORDER_ATOMIC);
   // TODO: Assemble Python into code variable.
-  var code = 'print("Hello World")';
+  var code = 'print("Hello World")\n';
   return code;
 };
